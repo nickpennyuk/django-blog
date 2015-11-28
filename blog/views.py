e026fd926039281cc9b5e9ffcbe802df1e424093
Nick Penny <nickpennyuk@gmail.com>
2015-11-28 11:07:13 +0000

    i cant remember what i changed

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 #client request > include app/urls.py into project/urls.py > 
 #app/urls.py define html files > views.py renders html and sends to client.
