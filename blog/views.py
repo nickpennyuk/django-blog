82d485cf602f76095914413bf2c6f4fcbf2eafd2
Nick Penny <nickpennyuk@gmail.com>
2015-11-29 22:51:34 +0000

    template extending

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {})
-
+	return render(request, 'blog/post_list.html', {'posts' : posts})
+	
 def post_detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'blog/post_detail.html', {'post': post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})   
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 #client request > include app/urls.py into project/urls.py > 
 #app/urls.py define html files > views.py renders html and sends to client.
