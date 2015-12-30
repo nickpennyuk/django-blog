f8b81e7e86382748cc3c7f935d0595d2163c2c01
Nick Penny <nickpennyk@gmail.com>
2015-12-30 15:37:28 +0000

    Personalisaton Updates

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,18 @@ from . import views
 from .forms import PostForm
 
 urlpatterns = [
+
+#  Home
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+#  Artist
     url(r'^artist/$', views.artist_list, name='artist_list'),
     url(r'^artist/new/$', views.artist_new, name='artist_new'),
     url(r'^artist/(?P<pk>[0-9]+)/edit/$', views.artist_edit, name='artist_edit'),
+#  Album
     url(r'^artist/album/$', views.album_list, name='album_list'),
     url(r'^artist/album/new/$', views.album_new, name='album_new'),
+
 ]
