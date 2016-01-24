ff71b6225f58a04ded62fb8852c81312688bdc26
Nick Penny <nickpennyuk@gmail.com>
2016-01-24 22:47:16 +0000

    24/01/2015

--- blog/views.py
+++ blog/views.py
@@ -43,25 +43,15 @@ class PostUpdateView(UpdateView):
         return reverse('post_detail', args=[self.object.pk])
 
 
-"""
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_form.html', {'form': form})
-"""
 
 class ArtistListView(ListView):
 
     template_name = 'blog/artist_list.html'
+    model = Artist
+
+
+class ArtistDetailView(DetailView):
+
     model = Artist
 
 
@@ -81,11 +71,6 @@ class ArtistUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('artist_list')
-
-
-class ArtistDetailView(DetailView):
-
-    model = Artist
 
 
 class AlbumCreateView(CreateView):
@@ -108,6 +93,21 @@ class AlbumUpdateView(UpdateView):
 
 
 """
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+
 def album_edit(request, pk):
     album = get_object_or_404(Album, pk=pk)
     if request.method == "POST":
