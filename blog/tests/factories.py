49769d32deddb8f183449e36b2733068ff841477
Patrick Kimber <code@pkimber.net>
2016-01-28 17:27:19 +0000

    switch tabs to spaces

--- blog/tests/factories.py
+++ blog/tests/factories.py
@@ -5,25 +5,25 @@ from django.contrib.auth.models import User
 
 class UserFactory(factory.django.DjangoModelFactory): # Called in PostFactory
 
-	class Meta:
-		model = User
+    class Meta:
+        model = User
 
 
 class PostFactory(factory.django.DjangoModelFactory):
 
-	class Meta:
-		model = Post
+    class Meta:
+        model = Post
 
-	author = factory.SubFactory(UserFactory)
-	
+    author = factory.SubFactory(UserFactory)
+    
 
 class ArtistFactory(factory.django.DjangoModelFactory):
 
-	class Meta:
-		model = Artist
+    class Meta:
+        model = Artist
 
 
 class AlbumFactory(factory.django.DjangoModelFactory):
 
-	class Meta:
-		model = Album
+    class Meta:
+        model = Album
